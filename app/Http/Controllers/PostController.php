f3053364d41abc07ab84ccc6ce513d09fa00c92e
BrotherlyHamlet16 <80749000+Clever-Niwagaba@users.noreply.github.com>
2025-04-23 13:27:13 +0300

    Updated Controller class

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         $post = Post::orderBy('created_at', 'desc')->get();
-        return view('posts.index', ['post' => $post]);
+        return view('posts.index', ['posts' => $post ?? collect()]);
     }
 
     // Create Post
@@ -30,16 +30,23 @@ class PostController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
-        $post = new Post;
+        // $post = new Post;
 
         $file_name = time() . '.' . request()->image->getClientOriginalExtension();
         request()->image->move(public_path('images'), $file_name);
 
-        $post->title = $request->title;
-        $post->description = $request->description;
-        $post->image = $file_name;
+        // $post->title = $request->title;
+        // $post->description = $request->description;
+        // $post->image = $file_name;
+
+        $post = new Post([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $file_name,
+        ]);
 
         $post->save();
+
         return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 }
